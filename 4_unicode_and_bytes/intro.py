7d55e3c769215b1724fb2c38dbf572d82f055c49
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2023-02-26 13:16:12 +0100

    Unicode and bytes intro - just basics, will back to this

--- 4_unicode_and_bytes/intro.py
+++ 4_unicode_and_bytes/intro.py
@@ -5,6 +5,13 @@ def intro_encoding_decoding() -> None:
     print(f'encoded UTF-8: {encoded} with len: {len(encoded)}')
     print(f'decoded encoded: {encoded.decode("utf8")}')
 
+def bytes_examples() -> None:
+    yellow_b = bytes('żółty', encoding='utf_8')
+    print(f'yello bytes: {yellow_b}')
+    print(f'First byte: {yellow_b[0]}')
+    print(f'First 3 bytes: {yellow_b[0:3]}')
 
 if __name__ == '__main__':
     intro_encoding_decoding()
+    bytes_examples()
+
